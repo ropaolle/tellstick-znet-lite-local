1f9dba11e23d1f33e5d3a9de5a65cd05f03e9046
ropaolle <ropaolle@gmail.com>
2018-02-28 11:03:51 +0100

    Refactor to pure function

--- src/components/DeviceList.jsx
+++ src/components/DeviceList.jsx
@@ -1,57 +1,51 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import map from 'lodash.map';
 import { Row, Col } from 'reactstrap';
 import Toggle from 'react-toggle';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
-class Devices extends Component {
-  render() {
-    const { devices, handleDeviceToggle, onSliderChange, handleDeviceDimmer } = this.props;
+const Devices = (props) => {
+  const { devices, handleDeviceToggle, onSliderChange, handleDeviceDimmer } = props;
 
-    return map(devices, (dev) => {
-      if (![6, 8, 18].includes(dev.id)) return '';
+  const toggle = device => (
+    <label htmlFor={`toggle-${device.id}`}>
+      <Toggle
+        checked={device.state === 1}
+        id={device.id.toString()}
+        onChange={handleDeviceToggle}
+      />
+    </label>);
 
-      return (
-        <div key={dev.id}>
-          <Row>
-            <Col>{dev.name}</Col>
-          </Row>
-          <Row>
-            <Col className="col-2">
-              <label htmlFor={`toggle-${dev.id}`}>
-                <Toggle
-                  checked={dev.state === 1}
-                  id={dev.id.toString()}
-                  onChange={handleDeviceToggle}
-                />
-              </label>
-            </Col>
-            <Col className="col-10">
-              <div className="slider">
-                <Slider
-                  min={1}
-                  max={255}
-                  value={Number(dev.statevalue)}
-                  disabled={!(dev.methods & 16)} // eslint-disable-line no-bitwise
-                  onChange={onSliderChange(dev.id)}
-                  onAfterChange={handleDeviceDimmer(dev.id)}
-                />
-              </div>
-            </Col>
-          </Row>
-        </div>
-      );
-    });
-  }
-}
+  const slider = device => (
+    <div className="slider">
+      <Slider
+        min={1}
+        max={255}
+        value={Number(device.statevalue)}
+        disabled={!(device.methods & 16)} // eslint-disable-line no-bitwise
+        onChange={onSliderChange(device.id)}
+        onAfterChange={handleDeviceDimmer(device.id)}
+      />
+    </div>);
 
-Devices.propTypes = {
-  devices: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  handleDeviceToggle: PropTypes.func.isRequired,
-  onSliderChange: PropTypes.func.isRequired,
-  handleDeviceDimmer: PropTypes.func.isRequired,
+  return map(devices, (device) => {
+    if (![6, 8, 18].includes(device.id)) return '';
+
+    return (
+      <div key={device.id}>
+        <Row><Col>{device.name}</Col></Row>
+        <Row>
+          <Col className="col-2">
+            {toggle}
+          </Col>
+          <Col className="col-10">
+            {slider}
+          </Col>
+        </Row>
+      </div>
+    );
+  });
 };
 
 export default Devices;
