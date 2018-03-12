29b2e5dddf7f4ba3bd9270437d19bd1d96c4fcf1
ropaolle <ropaolle@gmail.com>
2018-03-12 15:00:21 +0100

    Demo data added

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,7 @@ import Navbar from './Navbar';
 import AuthDialog from './AuthDialog';
 import Devices from './components/Devices';
 import telldusCommand from './utils/tellstick-znet-lite';
-
-const DEFAULT = {
-  expires: 1523214827000,
-  allowRenew: true,
-  devices: {
-    2: { id: 2, methods: 19, name: 'Stickpropp Dim Zwave', state: 2, statevalue: '', type: 'device', favorite: true },
-    6: {
-      id: 6,
-      methods: 19,
-      name: 'Sovrum Tak',
-      state: 2,
-      statevalue: '',
-      type: 'device',
-      favorite: true },
-    8: {
-      id: 8,
-      methods: 19,
-      name: 'Vardagsrum Tak',
-      state: 2,
-      statevalue: '',
-      type: 'device',
-      favorite: false,
-
-    },
-  },
-  sensors: {
-    1: { battery: 254, id: 1, model: 'temperature', name: 'Kylen', protocol: 'fineoffset', temp: '11.8', type: 'sensor', favorite: true },
-    7: { id: 7, lum: '2.0', model: 'n/a', name: 'Multisensor', protocol: 'zwave', temp: '30.6', type: 'sensor', favorite: true },
-    21: { battery: 254, humidity: '20', id: 21, model: 'temperaturehumidity', name: 'Tvlåda', protocol: 'fineoffset', temp: '23.9', type: 'sensor', favorite: true },
-  },
-};
+import demoData from './demoData';
 
 class App extends Component {
   state = {
@@ -54,9 +24,9 @@ class App extends Component {
     // eslint-disable-next-line promise/valid-params
     telldusCommand('init')
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         if (process.env.REACT_APP_MODE === 'DEMO') {
-          return this.setState(DEFAULT);
+          return this.setState(demoData);
         }
 
         if (!response.success) {
